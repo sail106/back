eb5fabf20143c335da09208d07df04b1e178bf18
김형민 <kimtuti@naver.com>
2024-01-09 17:39:53 +0900

    feat : #2 redis 설정

--- back/src/main/java/com/sail/back/global/config/RedisConfig.java
+++ back/src/main/java/com/sail/back/global/config/RedisConfig.java
@@ -3,7 +3,14 @@ package com.sail.back.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -12,7 +19,22 @@ public class RedisConfig {
 
     private final RedisProperties redisProperties;
 
-    //lettuce
+    // lettuce
+    @Bean
+    @Profile("dev")
+    public RedisConnectionFactory redisConnectionFactoryDev() {
+        return new LettuceConnectionFactory(
+                new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
+    }
 
+    @Bean
+    @Profile("dev")
+    public RedisTemplate<?, ?> redisTemplateDev() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactoryDev());   //connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
+        redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
+        return redisTemplate;
+    }
 
 }
